5f4b3fc4bf9a9df6a8bf7db76eb58056577802d3
Iseoluwa <iseoyedotun@gmail.com>
2022-06-20 13:05:00 +0100

    first commit

---
+++ 2-names.js
@@ -0,0 +1,5 @@
+const secret = 'SECRET NAME'
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john, peter}
---
+++ 6-modules.js
@@ -0,0 +1,8 @@
+const names = require('./2-names')
+const sayHi = require('./3-utils')
+// console.log(names)
+const data = require('./4-alternative')
+require('./5-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
